3ef0e3aaf1dc7fbd0e3e9c4e82e767ca4a9cc9b3
lororock <cristhianrosas159@hotmail.com>
2024-08-11 22:18:32 -0500

    add services nowPlay, upComing and topRated

--- src/services/getData.js
+++ src/services/getData.js
@@ -39,6 +39,47 @@ export const getPopulars = async () => {
   }
 };
 
+export const getNowPlaying = async () => {
+  try {
+    const response = await axiosInstance.get(
+      "movie/now_playing?language=en-US&page=1"
+    );
+    const { data } = response;
+    return data;
+  } catch (error) {
+    console.error("Error:", error.message);
+    throw error;
+  }
+};
+
+export const getUpComing = async () => {
+  try {
+    const response = await axiosInstance.get(
+      "movie/upcoming?language=en-US&page=1"
+    );
+    const { data } = response;
+    return data;
+  } catch (error) {
+    console.error("Error:", error.message);
+    throw error;
+  }
+};
+
+export const getTopRated = async () => {
+  try {
+    const response = await axiosInstance.get(
+      "movie/top_rated?language=en-US&page=1"
+    );
+    const { data } = response;
+    return data;
+  } catch (error) {
+    console.error("Error:", error.message);
+    throw error;
+  }
+};
+
+
+
 export const getGenders = async () => {
   try {
     const response = await axiosInstance.get("genre/movie/list?language=en");
