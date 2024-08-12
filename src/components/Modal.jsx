e29e9292f7491c05ac5648a7ade12dd209d932c6
lororock <cristhianrosas159@hotmail.com>
2024-08-12 11:02:15 -0500

    install sweetalert2 and add on modal

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import Swal from "sweetalert2";
+
 import PropTypes from "prop-types";
 import imgLogIn from "../assets/LogIn.png";
 import imgSingUp from "../assets/SignUp.png";
@@ -29,7 +31,40 @@ function Modal({ isOpen, onClose }) {
   const handleKeyDown = (event) => {
     if (event.key === "Enter") {
       event.preventDefault();
-      handleSubmit();
+      if (isLogIn) {
+        handleSubmit();
+        const Toast = Swal.mixin({
+          toast: true,
+          position: "top-end",
+          showConfirmButton: false,
+          timer: 3000,
+          timerProgressBar: true,
+          didOpen: (toast) => {
+            toast.onmouseenter = Swal.stopTimer;
+            toast.onmouseleave = Swal.resumeTimer;
+          },
+        });
+        Toast.fire({
+          icon: "success",
+          title: "Login in successfully",
+        });
+      } else if (!isLogIn) {
+        const Toast = Swal.mixin({
+          toast: true,
+          position: "top-end",
+          showConfirmButton: false,
+          timer: 6000,
+          timerProgressBar: true,
+          didOpen: (toast) => {
+            toast.onmouseenter = Swal.stopTimer;
+            toast.onmouseleave = Swal.resumeTimer;
+          },
+        });
+        Toast.fire({
+          icon: "success",
+          title: "Signed in successfully, confirm email",
+        });
+      }
     }
   };
 
