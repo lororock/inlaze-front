e87a54fcf21079a1dd3729b662f6dc6d461a6ee0
lororock <cristhianrosas159@hotmail.com>
2024-08-11 21:00:32 -0500

    add services and variables de entorno whit .map mainbanner

---
+++ src/services/axiosIntance.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+const apiKey = import.meta.env.VITE_APP_API_KEY
+const bearerToken = import.meta.env.VITE_APP_BEARER_TOKEN
+
+
+export const baseURL = 'https://api.themoviedb.org/3/';
+
+const axiosInstance = axios.create({
+  baseURL: baseURL,
+  headers: { Authorization: `Bearer ${bearerToken}` },
+  params: { apiKey },
+});
+
+export default axiosInstance;
---
+++ src/services/getData.js
@@ -0,0 +1,38 @@
+import axiosInstance from "./axiosIntance";
+
+const movieId = "106646";
+
+export const getMovie = async () => {
+  try {
+    const response = await axiosInstance.get(`movie/${movieId}`);
+    const movieData = response.data;
+    return movieData;
+  } catch (error) {
+    console.error("Error:", error);
+    throw error;
+  }
+};
+
+export const getMovies = async () => {
+  try {
+    const response = await axiosInstance.get(
+      "discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc"
+    );
+    const { data } = response;
+    return data;
+  } catch (error) {
+    console.error("Error:", error.message);
+    throw error;
+  }
+};
+
+export const getGenders = async () => {
+  try {
+    const response = await axiosInstance.get("genre/movie/list?language=en");
+    const { data } = response;
+    return data;
+  } catch (error) {
+    console.error("Error:", error);
+    throw error; // Rechazar la promesa si ocurre un error
+  }
+};
