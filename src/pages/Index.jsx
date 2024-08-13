90179e402ea1622742954d9efdc0161feb587032
lororock <cristhianrosas159@hotmail.com>
2024-08-12 21:53:41 -0500

    add postConfirmToken on index

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,20 +1,31 @@
-import { useEffect } from "react";
-
+import { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
+import { postConfirmToken } from "../services/userAuth.js";
+
 import MainBanner from "../components/MainBanner.jsx";
 import Categorias from "../components/categorias.jsx";
 
 function Index() {
   const location = useLocation();
+  const [confirmToken, setConfirmToken] = useState(null);
 
   useEffect(() => {
     const path = location.search;
-    const token = path.substring('/verify/'.length);
+    const token = path.substring("/verify/".length);
 
-    if (token) {
-      localStorage.setItem("authToken", token);
-    }
-  }, [location.search]);
+    localStorage.setItem("authToken", token);
+    const fetchMovieData = async () => {
+      try {
+        const data = await postConfirmToken(token);
+        setConfirmToken(data);
+        console.log(confirmToken);
+      } catch (error) {
+        console.error("Error al obtener los datos de la película:", error);
+      }
+    };
+
+    fetchMovieData();
+  });
   return (
     <>
       <MainBanner />
--- src/services/userAuth.js
+++ src/services/userAuth.js
@@ -1,12 +1,28 @@
 import axios from "axios";
 
 export const postRegisterUser = async (userData) => {
-    try {
-      const response = await axios.post('http://localhost:9000/auth/register', userData);
-      console.log('Usuario registrado:', response.data);
-      return response.data;
-    } catch (error) {
-      console.error('Error al registrar el usuario:', error.response.data);
-      throw error;
-    }
-  };
+  try {
+    const response = await axios.post(
+      "http://localhost:9000/auth/register",
+      userData
+    );
+    console.log("Usuario registrado:", response.data);
+    return response.data;
+  } catch (error) {
+    console.error("Error al registrar el usuario:", error.response.data);
+    throw error;
+  }
+};
+
+export const postConfirmToken = async (token) => {
+  try {
+    const response = await axios.post(
+      `http://localhost:9000/auth/confirm/${token}`
+    );
+    console.log("Usuario registrado:", response.data);
+    return response.data;
+  } catch (error) {
+    console.error("Error al registrar el usuario:", error.response.data);
+    throw error;
+  }
+};
