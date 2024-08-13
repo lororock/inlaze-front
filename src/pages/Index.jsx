f621223c17ea2f7caee55fa6a7f4ed8ea04516c3
lororock <cristhianrosas159@hotmail.com>
2024-08-12 21:19:34 -0500

    token email verify

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,7 +1,20 @@
+import { useEffect } from "react";
+
+import { useLocation } from "react-router-dom";
 import MainBanner from "../components/MainBanner.jsx";
 import Categorias from "../components/categorias.jsx";
 
 function Index() {
+  const location = useLocation();
+
+  useEffect(() => {
+    const path = location.search;
+    const token = path.substring('/verify/'.length);
+
+    if (token) {
+      localStorage.setItem("authToken", token);
+    }
+  }, [location.search]);
   return (
     <>
       <MainBanner />
